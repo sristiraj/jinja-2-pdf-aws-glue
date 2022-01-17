ae4723534f5bc3c0592080469a2ab16cea22d37e
sristiraj <sristiraj.a@gmail.com>
2022-01-17 22:41:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,18 +18,16 @@ import boto3
 
 #For glue invocation
 AWS_REGION = "us-east-1"
-args = getResolvedOptions(sys.argv, ["TEMPLATE_PATH","INPUT_DATA_PATH","OUTPUT_PDF_PATH","PART_SSN","GLUE_CONN_NAME","TMP_DIR_PATH","HEADER_QUERY_PATH","DETAIL_QUERY_PATH"])
+args = getResolvedOptions(sys.argv, ["TEMPLATE_PATH","INPUT_DATA_PATH","OUTPUT_PDF_PATH","PART_SSN","GLUE_CONN_NAME","TMP_DIR_PATH","HEADER_TABLE","DETAIL_TABLE"])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
 temp_pdf = "Report_{}.pdf".format(args["PART_SSN"].replace(" ","_"))
 print(temp_pdf)
-connection_redshift_options = {}
 
-def get_report_data_jdbc(self, tbl):
-        ddf = glueContext.create_dynamic_frame.from_options(connection_type="redshift", connection_options=connection_redshift_options)
-        return df
+
+
 
 def read_template(path):
     '''
@@ -49,10 +47,26 @@ def read_template(path):
             template_data = f.read()
     return template_data
 
-def get_report_data(input_data_path):
+def get_report_data(glue_conn_name, tmp_dir_path):
     import boto3
-    df_header = spark.read.format("csv").option("header","true").load(input_data_path+"/header").fillna("")
-    df_detail = spark.read.format("csv").option("header","true").load(input_data_path+"/detail").fillna("")
+    client = boto3.client('glue', region_name="us-east-1")
+    response = client.get_connection(Name=glue_conn_name)
+    connection_properties = response['Connection']['ConnectionProperties']
+    URL = connection_properties['JDBC_CONNECTION_URL']
+    url_list = URL.split("/")
+
+    host = "{}".format(url_list[-2][:-5])
+    port = url_list[-2][-4:]
+    database = "{}".format(url_list[-1])
+    user = "{}".format(connection_properties['USERNAME'])
+    pwd = "{}".format(connection_properties['PASSWORD'])
+    connection_redshift_options = {"url": URL, "user": user, "password": pwd, "redshiftTmpDir": tmp_dir_path} 
+    # df_header = spark.read.format("csv").option("header","true").load(input_data_path+"/header").fillna("")
+    # df_detail = spark.read.format("csv").option("header","true").load(input_data_path+"/detail").fillna("")
+    connection_redshift_options["dbtable"] = args["HEADER_TABLE"]
+    df_header = glueContext.create_dynamic_frame.from_options(connection_type="redshift", connection_options=connection_redshift_options)
+    connection_redshift_options["dbtable"] = args["DETAIL_TABLE"]
+    df_header = glueContext.create_dynamic_frame.from_options(connection_type="redshift", connection_options=connection_redshift_options)
     df_summed = df_detail.groupBy("PART_SSN").agg(round(sum("EMPLOYEE"),2).alias("EMPLOYEE_SUM"),round(sum("AUTOMATIC"),2).alias("AUTOMATIC_SUM"),round(sum("MATCHING"),2).alias("MATCHING_SUM"),round(sum("ROW_TOTAL"),2).alias("ROW_TOTAL_SUM"))
 
     list_data_header = list(map(lambda row: row.asDict(), df_header.collect()))
@@ -82,7 +96,7 @@ def write_pdf_s3(filename, path):
 template_data = read_template(args["TEMPLATE_PATH"])
 template = Template(template_data)
 
-list_data = get_report_data(args["INPUT_DATA_PATH"])
+list_data = get_report_data(args["GLUE_CONN_NAME"],args["TMP_DIR_PATH"])
 # print(type(list_data))
 template_out = template.render(list_data=list_data)
 
