8cf71bcdb21c2a81ce9c8f58057d6b86a535002b
sristiraj <sristiraj.a@gmail.com>
2022-02-07 16:51:48 +0530

    Update lambda_event_trigger.py

--- lambda_event_trigger.py
+++ lambda_event_trigger.py
@@ -8,15 +8,17 @@ import csv
 
 EVENT_FILE_CSV_DELIMIITER = "|"
 EVENT_FILE_CSV_QUOTE_CHAR = '"'
+
+trigger_switcher={"glue_trigger":"generate_PDF","glue_trigger1":"generate_PDF"}
+
 #Initiate loggger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 # Import Boto 3 for AWS Glue
 client = boto3.client('glue')
 
-# Variables for the job: 
-glueJobName = os.environ["glue_job_name"]
 
 def read_s3(s3_file):
     s3 = boto3.client("s3")
@@ -44,6 +46,7 @@ def read_s3_event_file(bucket_name, object_key):
 
 # Define Lambda function
 def lambda_handler(event, context):
+    logger.info(event)
     logger.info('## INITIATED BY EVENT: ')
     logger.info("Read Event Source")
     eventSource = event.get("Records",[{"eventSource":"unknown"}])[0].get("eventSource","unknown")
@@ -54,6 +57,12 @@ def lambda_handler(event, context):
         event_record = read_s3_event_file(bucket_name, object_key)   
         print(event_record)
         logger.info(f"Event Record: {event_record}".format())
+        logger.info("Checking glue job to trigger")
+        key_prefix = object_key[:object_key.find("/")]
+        # Variables for the job: 
+        glueJobName = trigger_switcher.get(key_prefix)
+        logger.info(f"Job to trigger {glueJobName}".format())
+        
         logger.info(f"Start Glue Job {glueJobName}".format())
         response = client.start_job_run(JobName = glueJobName, Arguments={"--PART_SSN":event_record[1], "--POST_DATE_START":event_record[2], "--POST_DATE_END":event_record[3]})
         logger.info('## STARTED GLUE JOB: ' + glueJobName)
