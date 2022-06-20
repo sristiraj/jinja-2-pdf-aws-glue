de00be889efe1724748c52159e073aa0d1752cd2
sristiraj <sristiraj.a@gmail.com>
2022-06-20 20:38:04 +0530

    Update date_formatter.py

--- date_formatter.py
+++ date_formatter.py
@@ -1,10 +1,10 @@
 list_data_detail1 = []
 for item in list_data_detail:
-	if str(item["PAY_DATE"]) is not None or str(item["PAY_DATE"]) != "":
+	if str(item["PAY_DATE"]) is not None and str(item["PAY_DATE"]) != "":
 		item_pay_date = datetime.strptime(str(item["PAY_DATE"]),"%Y-%m-%d").strftime("%m%d/%Y")
 	else:
 		item_pay_date = None
-	if str(item["POST_DATE"]) is not None or str(item["POST_DATE"]) != "":
+	if str(item["POST_DATE"]) is not None and str(item["POST_DATE"]) != "":
 		item_post_date = datetime.strptime(str(item["POST_DATE"]),"%Y-%m-%d").strftime("%m%d/%Y")
 	else:
 		item_post_date = None
